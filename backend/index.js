e036dea4cdc76dbc8e48b8cd67910ff34ecccf0b
Kururururururururu <72030465+Kururururururururu@users.noreply.github.com>
2023-10-13 21:35:00 +0200

    moved away from module

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import path from 'path'
+const path = require('path')
+const express = require('express')
 
 const app = express()
 const PORT = process.env.PORT || 80
