54777ad03cb7498d0e6c226ac81c8acab0ad955f
DanielZ1337 <danielz2nd@hotmail.com>
2023-11-23 11:57:32 +0100

    Update beer-containers.jsx

--- frontend/src/components/beer-components/container/beer-containers.jsx
+++ frontend/src/components/beer-components/container/beer-containers.jsx
@@ -1,13 +1,15 @@
-import BeerContainer from "./beer-container.jsx";
-import {beers} from "../../../constants/beers.js";
+import BeerContainer from './beer-container.jsx'
+import { beers } from '../../../constants/beers.js'
 
-
-export default function BeerContainers(){
-    return (
-        <div className='flex'>
-            {beers.map((_, index) => (
-                <BeerContainer key={index} />
-            ))}
-        </div>
-    )
+export default function BeerContainers() {
+	return (
+		<div className='flex'>
+			{beers.map((type, index) => (
+				<div key={index} className='flex flex-col'>
+					<BeerContainer />
+					<p className='text-2xl font-bold text-gray-900'>{type}</p>
+				</div>
+			))}
+		</div>
+	)
 }
