b72f0fafea69bb617fb29b1c88430dd7a388977f
DanielZ1337 <danielz2nd@hotmail.com>
2023-11-23 11:47:33 +0100

    make /pass-to-queue more robust :)

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.get('/api', async (req, res) => {
 		const stateCurrent = await opcua.read('StateCurrent')
 		const yeast = await opcua.read('Yeast')
 		const malt = await opcua.read('Malt')
-		const values = { wheat, barley, hops, yeast, malt,stateCurrent}
+		const values = { wheat, barley, hops, yeast, malt, stateCurrent }
 		const chunk = JSON.stringify({ values })
 		res.write(`data: ${chunk}\n\n`)
 	}, 1000)
@@ -38,30 +38,45 @@ app.get('/api', async (req, res) => {
 
 app.post('/pass-to-queue', async (req, res) => {
 	/*
-	const products = [
-		{ type: 'Pilser', id: 0 },
-		{ type: 'Wheat', id: 1 },
-		{ type: 'IPA', id: 2 },
-		{ type: 'Stout', id: 3 },
-		{ type: 'Ale', id: 4 },
-		{ type: 'Alcohol Free', id: 5 }
-	]
-	// receive request to process a beer type and an amount
-	beer_type = products.find(v => v.type === req.body.type).id	// search map where beer type equals product
-	*/
-	beer_type = req.body.type
-	beer_amount = req.body.amount
-	console.log(`Received request to brew ${beer_amount} of ${req.body.type}`)
+    const products = [
+        { type: 'Pilser', id: 0 },
+        { type: 'Wheat', id: 1 },
+        { type: 'IPA', id: 2 },
+        { type: 'Stout', id: 3 },
+        { type: 'Ale', id: 4 },
+        { type: 'Alcohol Free', id: 5 }
+    ]
+    // receive request to process a beer type and an amount
+    beer_type = products.find(v => v.type === req.body.type).id	// search map where beer type equals product
+    */
 
-	await opcuaClient.connect()
-	
+	try {
+		console.log(req.body)
+		// check if the request has the required fields
+		const { amount, type } = req.body
+		if (!amount || !type) throw new Error('Request is missing required fields')
 
-	if (await opcuaClient.brew(beer_type, beer_amount, 50)){	// send beer type request to queue
-		res.status(200) // if ok, return status code OK
-	} else {
-		res.status(418) // refuse to brew
+		const beer_type = type
+		// convert amount to number
+		const beer_amount = Number(amount)
+		if (isNaN(beer_amount)) throw new Error('Amount is not a number')
+
+		console.log(`Received request to brew ${beer_amount} of ${beer_type}`)
+
+		await opcuaClient.connect()
+
+		if (await opcuaClient.brew(beer_type, beer_amount, 50)) {
+			// send beer type request to queue
+			res.status(200) // if ok, return status code OK
+		} else {
+			res.status(418) // refuse to brew
+		}
+
+		res.send({ status: 'ok' })
+	} catch (error) {
+		console.error(error)
+		res.status(500).send({ error: error.message, status: 'error' })
 	}
-	
 })
 
 app.listen(PORT, function () {
--- frontend/src/components/beer-components/button/beer-buttons.jsx
+++ frontend/src/components/beer-components/button/beer-buttons.jsx
@@ -1,34 +1,33 @@
-import BeerButton from "./beer-button.jsx";
-import {beers} from "../../../constants/beers.js";
+import BeerButton from './beer-button.jsx'
+import { beers } from '../../../constants/beers.js'
 
 export default function BeerButtons() {
+	function brewBeer(beer) {
+		fetch('/pass-to-queue', {
+			method: 'POST',
+			body: JSON.stringify({
+				type: beer,
+				amount: 1,
+			}),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data)
+				// Handle data
+			})
+			.catch((err) => {
+				console.log(err.message)
+			})
+	}
 
-    function brewBeer(beer) {
-        fetch('/pass-to-queue', {
-            method: 'POST',
-            body: JSON.stringify({
-                type: beer,
-                amount: 1
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                // Handle data
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
-    }
-
-    return (
-        <div className={'flex gap-4'}>
-            {beers.map((beer, index) => (
+	return (
+		<div className={'flex gap-4'}>
+			{beers.map((beer, index) => (
                 <BeerButton key={index} beer={beer} brewBeer={() => brewBeer(beer)}/>
             ))}
-        </div>
-    )
+		</div>
+	)
 }
