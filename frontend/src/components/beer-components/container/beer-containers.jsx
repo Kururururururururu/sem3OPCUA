d291d15906ee58af7c35fc5056201b480c6b4b9a
DanielZ1337 <danielz2nd@hotmail.com>
2023-11-30 13:49:08 +0100

    y

--- frontend/src/components/beer-components/container/beer-containers.jsx
+++ frontend/src/components/beer-components/container/beer-containers.jsx
@@ -1,13 +1,46 @@
 import BeerContainer from './beer-container.jsx'
 import { beers } from '../../../constants/beers.js'
+import { useEffect, useState } from 'react'
+
+const inventoryItems = ['wheat', 'barley', 'hops', 'yeast', 'malt']
 
 export default function BeerContainers() {
+	const [inventory, setInventory] = useState()
+
+	useEffect(() => {
+		const getInventory = async () => {
+			const source = new EventSource('/api/inventory')
+
+			source.onmessage = function (event) {
+				console.log('event', event.data)
+				const parsedData = JSON.parse(event.data)
+				setInventory(parsedData)
+			}
+		}
+
+		getInventory()
+	}, [])
+
+	const getPercent = (inventoryItem) => {
+		if (!inventory) {
+			return 0
+		}
+
+		const max = 35000
+		// filter from the inventory object, where the key is the string in the inventoryItems array
+		const current = inventory[inventoryItem]
+		const percent = (current / max) * 100
+		return percent
+	}
 	return (
 		<div className='flex'>
-			{beers.map((beer, index) => (
-				<div key={index} className='flex flex-col'>
-					<BeerContainer />
-					<p className='text-2xl font-bold text-gray-900'>{beer.type}</p>
+			{inventoryItems.map((inventoryItem, index) => (
+				<div
+					key={index}
+					className='flex flex-col'
+				>
+					<BeerContainer percent={getPercent(inventoryItem)} />
+					<p className='text-2xl font-bold text-gray-900'>{inventoryItem}</p>
 				</div>
 			))}
 		</div>
