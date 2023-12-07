7f7269e9320cf65ad517c7cf74b2a669d2a7c022
DanielZ1337 <danielz2nd@hotmail.com>
2023-12-07 13:37:49 +0100

    deny brew when state is 6 (when it is brewing)

--- backend/OPC/OPC_client.js
+++ backend/OPC/OPC_client.js
@@ -264,9 +264,6 @@ module.exports = {
 				if (StopReason === 10 || StopReason === 11) {
 					await module.exports.maintenence()
 				}
-				while (currentState !== PackMLStateOptions.Idle) {
-					await new Promise((resolve) => setTimeout(resolve, 1000))
-				}
 				await module.exports.write(node.variable, node.value, node.dataType)
 			}
 
